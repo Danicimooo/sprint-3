2e2d9ea395bd383b33f3f5c7be4a2ed714a62808
Danicimooo <140618020+Danicimooo@users.noreply.github.com>
2023-07-28 14:44:03 +0600

    Update ViewController.swift

--- sprint 3/ViewController.swift
+++ sprint 3/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    let counterLabel = UILabel()
+    let button = UIButton(type: .system)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Настройка лейбла
+        counterLabel.frame = CGRect(x: 50, y: 100, width: 200, height: 30)
+        counterLabel.text = "Значение счётчика: \(counter)"
+        view.addSubview(counterLabel)
+
+        // Настройка кнопки
+        button.frame = CGRect(x: 50, y: 150, width: 200, height: 50)
+        button.setTitle("Нажми меня", for: .normal)
+        button.addTarget(self, action: #selector(incrementCounter), for: .touchUpInside)
+        view.addSubview(button)
     }
 
-
+    @objc func incrementCounter() {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
 }
 
